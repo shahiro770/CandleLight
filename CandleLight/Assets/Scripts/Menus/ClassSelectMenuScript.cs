5a14f2f86e06d151a8e77e1c7c87b9751aa16465
Shahir Chowdhury <shahirchowdhury9800@gmail.com>
2019-01-25 16:37:38 -0500

    Added character select menu and some keyboard input navigation

---
+++ CandleLight/Assets/Scripts/Menus/ClassSelectMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ClassSelectMenuScript : MonoBehaviour {
+   
+    public GameObject firstToSelect;
+    //GameObject lastSelected = firstSelected;
+    EventSystem es;
+
+    void OnEnable() {
+        es = EventSystem.current;
+        es.SetSelectedGameObject(firstToSelect);
+        firstToSelect.GetComponent<Button>().OnSelect(null);
+    }
+}
