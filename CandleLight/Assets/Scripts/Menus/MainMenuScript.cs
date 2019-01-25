e1f8a5c3a00e41d4d4dd5e6c8ad330fc7e1cdf5c
Shahir Chowdhury <shahirchowdhury@Shahirs-MacBook-Pro.local>
2019-01-25 16:37:38 -0500

    Added character select menu and some keyboard input navigation

---
+++ CandleLight/Assets/Scripts/Menus/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+    
+    public GameObject firstToSelect;
+    EventSystem es;
+
+    void OnEnable() {
+        es = EventSystem.current;
+
+        es.SetSelectedGameObject(firstToSelect);
+        firstToSelect.GetComponent<Button>().OnSelect(null);
+    }
+
+     public void QuitGame() {
+        Application.Quit();
+    }
+}
+
+
+//TO DO:
+//Make unity script that refocuses on the last ui element selected in the event user clicks on a non game object
